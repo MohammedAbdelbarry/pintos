f211f3c6f014411ccc1be0dbd9a77d69ec9e53fe
Mohammed Abdelbarry <m7md337@gmail.com>
2017-11-30 02:22:21 +0200

    Implemented fixed-point operations

---
+++ lib/kernel/fixed_point.h
@@ -0,0 +1,38 @@
+#ifndef __LIB_KERNEL_FIXED_POINT_H
+#define __LIB_KERNEL_FIXED_POINT_H
+
+/* Number of bits used to represent the fractional
+   part of the fixed point number. */
+#define DECIMAL_BITS 14
+/* The shift added to the integer number to convert
+   it to fixed point */
+#define SCALE (1 << DECIMAL_BITS)
+/* Converts an integer number to fixed point by shifting
+   its bits to the left $DECIMAL_BITS times */
+#define FIXED_POINT(n) ((n) * SCALE)
+/* Converts a fixed point number to integer by shifting
+   its bits to the right $DECIMAL_BITS times */
+#define INTEGER(x) ((x) / SCALE)
+/* Adds two fixed point numbers, which is essentially
+   the same as adding two integers */
+#define ADD(x, y) ((x) + (y))
+/* Subtracts y from x where y and x are fixed point nubmers.
+   This operation is the same as subtracting two integers */
+#define SUB(x, y) ((x) + (y))
+/* Converts n to fixed point then adds it to x */
+#define ADD_INT(x, n) ((x) + (n) * SCALE)
+/* Converts n to fixed point then subtracts it from x */
+#define SUB_INT(x, n) ((x) - (n) * SCALE)
+/* Multiplies two fixed point numbers */
+#define MUL(x, y) (((int64_t)(x)) * (y) / SCALE)
+/* Multiplies a fixed point number 'x' by an int 'n'.
+  The result is a fixed point number */
+#define MUL_INT(x, n) ((x) * (n))
+/* Divides two fixed point numbers or two ints and converts
+   the result to fixed point */
+#define DIV(x, y) (((int64_t)(x)) * SCALE / (y))
+/* Divides a fixed point number 'x' by an int 'n'.
+  The result is a fixed point number */
+#define DIV_INT(x, n) ((x) / (n))
+
+#endif // fixed_point.h
