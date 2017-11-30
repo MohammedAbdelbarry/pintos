b03b0bbdca4966632b5e5c09d264d6c65962ca18
Mohammed Abdelbarry <m7md337@gmail.com>
2017-11-30 21:42:11 +0200

    Added the ROUND Macro

--- lib/kernel/fixed_point.h
+++ lib/kernel/fixed_point.h
@@ -5,34 +5,36 @@
    part of the fixed point number. */
 #define DECIMAL_BITS 14
 /* The shift added to the integer number to convert
-   it to fixed point */
+   it to fixed point. */
 #define SCALE (1 << DECIMAL_BITS)
 /* Converts an integer number to fixed point by shifting
-   its bits to the left $DECIMAL_BITS times */
+   its bits to the left $DECIMAL_BITS times. */
 #define FIXED_POINT(n) ((n) * SCALE)
 /* Converts a fixed point number to integer by shifting
-   its bits to the right $DECIMAL_BITS times */
+   its bits to the right $DECIMAL_BITS times. */
 #define INTEGER(x) ((x) / SCALE)
 /* Adds two fixed point numbers, which is essentially
-   the same as adding two integers */
+   the same as adding two integers. */
 #define ADD(x, y) ((x) + (y))
 /* Subtracts y from x where y and x are fixed point nubmers.
-   This operation is the same as subtracting two integers */
+   This operation is the same as subtracting two integers. */
 #define SUB(x, y) ((x) + (y))
-/* Converts n to fixed point then adds it to x */
+/* Converts n to fixed point then adds it to x. */
 #define ADD_INT(x, n) ((x) + (n) * SCALE)
-/* Converts n to fixed point then subtracts it from x */
+/* Converts n to fixed point then subtracts it from x. */
 #define SUB_INT(x, n) ((x) - (n) * SCALE)
 /* Multiplies two fixed point numbers */
 #define MUL(x, y) (((int64_t)(x)) * (y) / SCALE)
 /* Multiplies a fixed point number 'x' by an int 'n'.
-  The result is a fixed point number */
+  The result is a fixed point number. */
 #define MUL_INT(x, n) ((x) * (n))
 /* Divides two fixed point numbers or two ints and converts
-   the result to fixed point */
+   the result to fixed point. */
 #define DIV(x, y) (((int64_t)(x)) * SCALE / (y))
 /* Divides a fixed point number 'x' by an int 'n'.
-  The result is a fixed point number */
+  The result is a fixed point number. */
 #define DIV_INT(x, n) ((x) / (n))
+/* Rounds a fixed point number x to the nearest integer. */
+#define ROUND(x) (((x) > 0) ? ((x) + SCALE / 2) / SCALE : ((x) - SCALE / 2) / SCALE) 
 
 #endif // fixed_point.h
