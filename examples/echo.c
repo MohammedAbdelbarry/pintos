89451cc44433f9eab913db9ecb52d61dae8e60e9
Mohammed Abdelbarry <m7md337@gmail.com>
2018-01-16 12:44:57 +0200

    Implements Argument Passing

--- examples/echo.c
+++ examples/echo.c
@@ -5,7 +5,7 @@ int
 main (int argc, char **argv)
 {
   int i;
-
+  //while (argc == 4 && !strcmp(argv[0], "echo") && !strcmp(argv[1], "x") && !strcmp(argv[2], "y") && !strcmp(argv[3], "z"));
   for (i = 0; i < argc; i++)
     printf ("%s ", argv[i]);
   printf ("\n");
