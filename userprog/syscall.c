cff69b226d5cafab7340824b862428b60741c981
MohamedAshrafTolba <tolba96mohamed@gmail.com>
2018-01-16 16:03:29 +0200

    Implemented system calls handler.

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -6,6 +6,20 @@
 
 static void syscall_handler (struct intr_frame *);
 
+static void halt (void);
+static void exit (int);
+static pid_t exec (const char *);
+static int wait (pid_t);
+static bool create (const char *, unsigned);
+static bool remove (const char *);
+static int open (const char *);
+static int filesize (int);
+static int read (int, void *, unsigned);
+static int write (int, const void *, unsigned);
+static void seek (int, unsigned);
+static unsigned tell (int);
+static void close (int);
+
 void
 syscall_init (void) 
 {
@@ -13,8 +27,130 @@ syscall_init (void)
 }
 
 static void
-syscall_handler (struct intr_frame *f UNUSED) 
+syscall_handler (struct intr_frame *f) 
 {
-  printf ("system call!\n");
-  thread_exit ();
+  uint32_t *esp = f->esp;
+  int syscall_num = *esp;
+  switch (syscall_num)
+    {
+      case SYS_HALT:
+        halt ();
+        break;
+      case SYS_EXIT:
+        exit (*(esp + 1));
+        break;
+      case SYS_EXEC:
+        f->eax = exec ((char *) *(esp + 1));
+        break;
+      case SYS_WAIT:
+        f->eax = wait ((pid_t) *(esp + 1));
+        break;
+      case SYS_CREATE:
+        f->eax = create ((char *) *(esp + 1), *(esp + 2));
+        break;
+      case SYS_REMOVE:
+        f->eax = remove ((char *) *(esp + 1));
+        break;
+      case SYS_OPEN:
+        f->eax = open ((char *) *(esp + 1));
+        break;
+      case SYS_FILESIZE:
+        f->eax = filesize (*(esp + 1));
+        break;
+      case SYS_READ:
+        f->eax = read (*(esp + 1), (void *) *(esp + 2), *(esp + 3));
+        break;
+      case SYS_WRITE:
+        f->eax = write (*(esp + 1), (void *) *(esp + 2), *(esp + 3));
+        break;
+      case SYS_SEEK:
+        seek (*(esp + 1), *(esp + 2));
+        break;
+      case SYS_TELL:
+        f->eax = tell (*(esp + 1));
+        break;
+      case SYS_CLOSE:
+        close (*(esp + 1));
+        break;
+      default:
+        break;
+    }
+}
+
+static void
+halt (void)
+{
+
+}
+
+static void
+exit (int status)
+{
+
+}
+
+static pid_t
+exec (const char *cmd_line)
+{
+  return 0;
+}
+
+static int
+wait (pid_t pid)
+{
+  return 0;
+}
+
+static bool
+create (const char *file, unsigned initial_size)
+{
+  return false;
+}
+
+static bool
+remove (const char *file)
+{
+  return false;
+}
+
+static int
+open (const char *file)
+{
+  return 0;
+}
+
+static int
+filesize (int fd)
+{
+  return 0;
+}
+
+static int
+read (int fd, void *buffer, unsigned size)
+{
+  return 0;
+}
+
+static int
+write (int fd, const void *buffer, unsigned size)
+{
+  return 0;
+}
+
+static void
+seek (int fd, unsigned position)
+{
+
+}
+
+static unsigned
+tell (int fd)
+{
+  return 0;
+}
+
+static void
+close (int fd)
+{
+
 }
